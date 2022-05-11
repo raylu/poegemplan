2286beb0a187617c39e67cea313fd4f8ef611a6b
raylu <90059+raylu@users.noreply.github.com>
2022-05-10 22:05:05 -0700

    show plan

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 		event.preventDefault();
 		const short = document.querySelector('form#pob input[name="short"]').value;
 		load(short);
-		history.pushState({}, '', 'pob/' + short);
+		history.pushState({}, '', '/pob/' + short);
 	});
 
 	if (window.location.pathname.substr(0, 5) === '/pob/') {
@@ -16,19 +16,30 @@
 
 	async function load(short) {
 		const pobRes = await fetch('/pob/raw/' + short);
-		const code = await pobRes.json();
-		console.log(code);
+		const build = await pobRes.json();
 
 		if (quests === null) {
 			const questsRes = await questsPromise;
 			quests = await questsRes.json();
 		}
-
 		const main = document.querySelector('main');
+		main.innerHTML = '';
 		for (const quest of quests) {
 			const section = document.createElement('section');
+			section.id = quest;
 			section.innerText = quest;
 			main.appendChild(section);
+		}
+
+		for (const gem of build['gems']) {
+			for (const quest of gem['quests']) {
+				if (quest['classes'] === 'All' || quest['classes'].indexOf(build['class']) !== -1) {
+					const div = document.createElement('div');
+					div.innerText = gem['name'];
+					main.querySelector('#' + quest['name']).appendChild(div);
+					break;
+				}
+			}
 		}
 	}
 })();
