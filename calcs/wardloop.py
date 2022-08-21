02fa69e617c78382822a7a3177591536e67b37a6
raylu <90059+raylu@users.noreply.github.com>
2022-08-21 00:24:29 -0700

    move to poecalc repo

--- calcs/wardloop.py
+++
@@ -1,8 +0,0 @@
-import stats
-
-def analyze(account, character):
-	char_stats = stats.fetch_stats(account, character)
-	print(char_stats)
-
-if __name__ == '__main__':
-	analyze('raylu', 'rayluloop')
