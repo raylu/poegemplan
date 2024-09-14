719ab58e639f29fca4de2a571569e62ea1e53676
raylu <90059+raylu@users.noreply.github.com>
2024-09-14 10:31:14 -0700

    handle transfigured gems

--- data.py
+++ data.py
@@ -6,21 +6,30 @@ def main():
 	res = httpx.get('https://poegems.com/json')
 	res.raise_for_status()
 
-	gems = {}
-	for gem in res.json():
-		name = gem['name']
+	input_gems = {gem['uuid']: gem for gem in res.json()}
+
+	output_gems = {}
+	for gem in input_gems.values():
+		name: str = gem['name']
 		if name.endswith('s Mark'):
 			name = name[:-len('s Mark')] + "'s Mark"
-		quests = gem['quests']
-		for quest in quests:
+
+		if base_gem := gem.get('baseGem'):
+			input_quests: list[dict] = input_gems[base_gem]['quests']
+		else:
+			input_quests = gem['quests']
+		output_quests = []
+		for quest in input_quests:
+			quest = quest.copy()
 			if quest['classes'].startswith('All, Classes'):
 				quest['classes'] = 'All'
 			else:
 				quest['classes'] = quest['classes'].split(', ')
-		gems[name] = {'quests': quests, 'src': gem['src']}
+			output_quests.append(quest)
+		output_gems[name] = {'quests': output_quests, 'src': gem['src']}
 
 	with open('gems.json', 'w', encoding='utf-8') as f:
-		json.dump(gems, f)
+		json.dump(output_gems, f)
 
 if __name__ == '__main__':
 	main()
